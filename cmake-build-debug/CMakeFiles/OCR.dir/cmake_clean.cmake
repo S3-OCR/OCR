a3325432a4d2a4faf7a6dde34a885b9aa35e62fb
Low_battery <marile.lin@epita.fr>
2018-10-09 07:27:09 -0700

    Test
    Contient un makefile et un main (qui ne contient pas grand chose pour le moment mais bref XD)

---
+++ cmake-build-debug/CMakeFiles/OCR.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OCR.dir/main.c.o"
+  "OCR.pdb"
+  "OCR"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OCR.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
